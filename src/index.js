9b1e0bba349d5d02cebcfb1a6368335fdb64ad96
Elkhan <is.elxan@gmail.com>
2022-05-23 01:21:51 +0400

    feature: add request response inteceptors

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import axios from 'axios';
 
 axios.interceptors.request.use(request => {
   console.log(request);
+  return request;
+});
+
+axios.interceptors.response.use(response => {
+  console.log(response);
+  return response;
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
