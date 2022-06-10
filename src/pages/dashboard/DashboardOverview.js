ebd957dc43e9bfb2fffb006e27b4f30b1aa9658d
Elkhan <is.elxan@gmail.com>
2022-06-11 02:40:27 +0400

    feature: fix style

--- src/pages/dashboard/DashboardOverview.js
+++ src/pages/dashboard/DashboardOverview.js
@@ -1,26 +1,26 @@
 
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCashRegister, faChartLine, faFolderPlus, faTicketAlt, faHistory, faBook} from '@fortawesome/free-solid-svg-icons';
-import { Col, Row} from '@themesberg/react-bootstrap';
+import { faCashRegister, faChartLine, faFolderPlus, faTicketAlt, faClock, faBook } from '@fortawesome/free-solid-svg-icons';
+import { Col, Row } from '@themesberg/react-bootstrap';
 
 import { CounterWidget, CircleChartWidget, SalesValueWidget, SalesValueWidgetPhone, AcquisitionWidget } from "../../components/Widgets";
 import { trafficShares } from "../../data/charts";
 import { getAllTickets, getAllCategories, getTicketCountByCategory } from "../../api/ThesisApi";
-import axios from "axios";
+import moment from "moment-timezone";
 
 export default () => {
   const [tickets, setTickets] = React.useState({ tickets: [] });
   const [categories, setCategories] = React.useState({ categories: [] });
   // const [bugsCount, setBugsCount] = React.useState
-  
+
   React.useEffect(() => {
     try {
       getAllTickets().then(res => {
-        setTickets({tickets: res.body});
+        setTickets({ tickets: res.body });
       });
     }
-    catch(e) {
+    catch (e) {
       console.log("error", e);
     }
     //
@@ -28,42 +28,47 @@ export default () => {
       getAllCategories().then(res => {
         console.log(res.body);
         let cats = res.body;
-       
+
         cats.forEach(element => {
-          
+
           getTicketCountByCategory(element.name).then(res => {
             // console.log(res);
             element.count = res.body;
-            setCategories({categories: cats});
+            setCategories({ categories: cats });
           });
-          
+
         });
-        
-        
-        
+
+
+
       });
     }
-    catch(e) {
+    catch (e) {
       console.log("error", e);
     }
   }, [])
 
   // console.log(categories);
-  let totalCounts = 0;
+  let totalTicketsCounts = 0;
   categories.categories.forEach(element => {
-    totalCounts += element.count;
+    totalTicketsCounts += element.count;
   });
   // console.log(totalCounts);
 
+  let totalTimeSpent = 0;
+  tickets.tickets.forEach(element => {
+    totalTimeSpent = moment(element.endTime).diff(moment(element.startTime), 'minutes') + totalTimeSpent;
+  });
+
   return (
     <>
-       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
         <div className="d-block mb-4 mb-md-0">
           <h4>Dashboard</h4>
           <p className="mb-0">Feedback Tool web analytics dashboard.</p>
         </div>
         <div className="btn-toolbar mb-2 mb-md-0">
-         
+
         </div>
       </div>
 
@@ -71,12 +76,33 @@ export default () => {
         <Col xs={12} sm={6} xl={4} className="mb-4">
           <CounterWidget
             category="All Feedback Tickets"
-            title={ `${tickets.tickets.length}`}
+            title={`${tickets.tickets.length}`}
             icon={faTicketAlt}
             iconColor="shape-secondary"
           />
         </Col>
 
+       
+
+        
+
+        <Col xs={12} sm={6} xl={4} className="mb-4">
+          <CounterWidget
+            category="Total minutes spent"
+            title={totalTimeSpent}
+            icon={faClock}
+            iconColor="shape-secondary"
+          />
+        </Col>
+
+        <Col xs={12} sm={6} xl={4} className="mb-4">
+          <CounterWidget
+            category="Tickets without Category"
+            title={tickets.tickets.length - totalTicketsCounts}
+            icon={faFolderPlus}
+            iconColor="shape-secondary"
+          />
+        </Col>
         <Col xs={12} sm={6} xl={4} className="mb-4">
           <CounterWidget
             category="All Categories"
@@ -85,17 +111,8 @@ export default () => {
             iconColor="shape-secondary"
           />
         </Col>
-
-        <Col xs={12} sm={6} xl={4} className="mb-4">
-          <CounterWidget
-            category="Tickets without Category"
-            title={tickets.tickets.length - totalCounts}
-            icon={faFolderPlus}
-            iconColor="shape-secondary"
-          />
-        </Col>
-
-
+        </Row>
+        <Row className="justify-content-md-center">
         {
           categories.categories.map((element, index) => (
             <Col xs={12} sm={6} xl={4} className="mb-4" key={`id-${index}`}>
@@ -108,9 +125,9 @@ export default () => {
             </Col>
           ))
         }
-        
 
-        
+
+
       </Row>
     </>
   );
